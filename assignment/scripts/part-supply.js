4a388545c470c9409c7c8a02085a0a051896916b
Leila Sadr <leilasadr54@gmail.com>
2023-03-11 20:29:52 -0800

    updated last Q answer

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -97,3 +97,15 @@ console.log(`${totalParts}`);
 //    no more boxes can be filled.
 //    Then log how many boxes were filled, and how many parts are left over.
 console.log('9. Filling boxes with a "while" loop');
+
+totalParts = 572;
+let boxCapacity = 7;
+let boxesFilled = 0;
+
+while (totalParts >= boxCapacity) {
+  totalParts -= boxCapacity;
+  boxesFilled++;
+} // end while filled 81
+
+console.log(`Boxes filled: ${boxesFilled}`);
+console.log(`Parts left over: ${totalParts}`);
